0f8bb1727360f0ccea6a1a543c963268875594ac
Jitgitbit <rowowolf@gmail.com>
2020-08-20 12:16:30 +0200

    exploring with kotlin syntax, (pretty standard)

--- app/src/main/java/com/example/kotlinfun/LearningKotlin.kt
+++ app/src/main/java/com/example/kotlinfun/LearningKotlin.kt
@@ -1,5 +1,21 @@
 package com.example.kotlinfun
 
 fun main() {
-    print("hello kotlin world")
+    print("hello kotlin world ")
+
+    var age = 29
+    var candy = "Fun Dip"
+    age = 30
+
+    var pi: Double = 3.14
+    var radius = 5
+    var c = pi * radius * 2
+
+    print(c)
+
+    var weight = 78
+    weight += 5
+
+    print(" ")
+    print(weight)
 }
